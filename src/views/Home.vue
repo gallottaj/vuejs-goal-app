cc3a7725a97bcc37a02fb23714aa32e7310d653e
Joe Gallotta <joeygallotta@gmail.com>
2018-11-21 19:47:56 -0600

    setup axios requests to validate goals and users data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="home">
+    <div class="home">
+   <!--  <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <h1>{{ message }}</h1>
+    <div v-for="goal in goals">
+      <h3>{{ goal.goal_name }}</h3>
+      <h4>{{ goal.due_date }}</h4>
+      <hr>
+    </div>
   </div>
 </template>
 
@@ -13,16 +20,22 @@ var axios = require('axios');
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      goals: [],
+      users: []
     };
   },
   created: function() {
     console.log('in the created function');
-    console.log(this);
-    axios.get('http://localhost:3000/api/goals').then(function(response) {
+    axios.get('http://localhost:3000/api/goals').then(response => {
       console.log(response.data);
-      this.people = response.data;
-    }.bind(this));
+      this.goals = response.data;
+    }),
+    console.log('in the created function');
+    axios.get('http://localhost:3000/api/users').then(response => {
+      console.log(response.data);
+      this.users = response.data;
+    });
   },
   methods: {},
   computed: {}
