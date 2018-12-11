4ccedd6e098f6fe20d8271511b003742d3d389f4
Joe Gallotta <joeygallotta@gmail.com>
2018-12-10 20:20:05 -0600

    style journal create page and form

--- src/views/JournalsCreate.vue
+++ src/views/JournalsCreate.vue
@@ -1,28 +1,68 @@
 <template>
   <div class="create journal">
-    <div class="container">
-      <form v-on:submit.prevent="submit()">
-        <h1>Create a journal </h1>
-        <div class="form-group">
-          <label>entry_type:</label>
-          <input type="text" class="form-control" v-model="entry_type">
-        </div>
-        <div class="form-group">
-          <label>entry_body:</label>
-          <input type="text" class="form-control" v-model="entry_body">
-        </div>
-        <div class="form-group">
-          <label>entry_title:</label>
-          <input type="text" class="form-control" v-model="entry_title">
-        </div>     
-        <div class="form-group">
-          <label>user_id:</label>
-          <input type="text" class="form-control" v-model="user_id">
-        </div>
-        <input type="submit" class="btn btn-primary" value="Submit">
-      </form>
-    </div>
-    <a v-bind:href="'/#/goals-create'" class="btn btn-primary">Create a new goal</a>
+        <!-- CONTENT --------------------------------------------------------------------------------->
+        <!-- Intro Section -->
+        <section class="inner-intro bg-image overlay-light parallax parallax-background1" data-background-img="img/full/02.jpg">
+            <div class="container">
+                <div class="row title">
+                    <h2 class="h2">Journal Entry</h2>
+                    <div class="page-breadcrumb">
+                        <a>Home</a>/<span>Journal</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="clearfix"></div>
+        <!-- End Intro Section -->
+
+        <!-- Contact Section -->
+        <section class="ptb ptb-sm-80">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 offset-md-3 text-center">
+                        <h3>Create New Entry</h3>
+                        <p class="lead">Nullam dictum felis eu pede mollis pretium leo eget bibendum sodales augue velit cursus. tellus eget condimentum rhoncus sem quam semper libero.</p>
+                    </div>
+                </div>
+                <div class="spacer-75"></div>
+                <div class="row">
+                    <div class="col-md-6 offset-md-3">
+                        <!-- Contact FORM -->
+                        <form v-on:submit.prevent="submit()">
+                            <!-- IF MAIL SENT SUCCESSFULLY -->
+                            <h6 class="successContent">
+                                <i class="fa fa-check left" style="color: #5cb45d;"></i>Your message has been sent successfully.
+                            </h6>
+                            <!-- END IF MAIL SENT SUCCESSFULLY -->
+
+                            <!-- MAIL SENDING UNSUCCESSFULL -->
+                            <h6 class="errorContent">
+                                <i class="fa fa-exclamation-circle left" style="color: #e1534f;"></i>There was a problem validating the form please check!
+                            </h6>
+                            <!-- END MAIL SENDING UNSUCCESSFULL -->
+
+                            <div class="form-field-wrapper">
+                                <input class="input-sm form-full" id="form-name" type="text" name="form-name" placeholder="journal title" required v-model="entry_type">
+                            </div>
+                            <div class="form-field-wrapper">
+                                <input class="input-sm form-full" id="form-name" type="text" name="form-name" placeholder="your user id" required v-model="user_id">
+                            </div>
+
+                            <div class="form-field-wrapper">
+                                <textarea class="form-full" id="form-message" rows="7" name="form-message" placeholder="a brief description" required></textarea>
+                            </div>
+
+                            <button class="btn btn-md btn-black form-full" type="submit" id="form-submit" name="submit">Submit</button>
+                        </form>
+                        <!-- END Contact FORM -->
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Contact Section -->
+
+        <!-- End CONTENT ------------------------------------------------------------------------------>
+
   </div>
 </template>
 
@@ -58,7 +98,7 @@ export default {
         axios
           .post('http://localhost:3000/api/journals', params)
           .then(response => {
-            this.$router.push("/journals");
+            this.$router.push("/home");
           // })
           // .catch(error => {
           //   this.errors = error.response.data.errors;
