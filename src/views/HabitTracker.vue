6aedf354427fde47be0b506c805c925b7fc83734
Joe Gallotta <joeygallotta@gmail.com>
2018-12-17 19:34:18 -0600

    add highchart to habittracker page

--- src/views/HabitTracker.vue
+++ src/views/HabitTracker.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div id="container" style="width:100%; height:400px;"></div>
   </div>
 </template>
 
@@ -8,13 +9,45 @@
 </style>
 
 <script>
+
+var Highcharts = require('highcharts');
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      journals: {}
     };
   },
-  created: function() {},
+  mounted: function() {
+    $(function() { 
+      var myChart = Highcharts.chart('container', {
+        chart: {
+          type: 'pie'
+        },
+        title: {
+          text: 'Fruit Consumption'
+        },
+        xAxis: {
+          categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+          title: {
+            text: 'Fruit eaten'
+          }
+        },
+        series: [{
+          name: 'Jane',
+          data: [1, 0, 4]
+        }, {
+          name: 'John',
+          data: [5, 7, 3]
+        }]
+      });
+    });
+  },
+  function() {
+  },
   methods: {},
   computed: {}
 };
